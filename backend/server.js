52d1dddd9db2125f86254176e32b16c040db7c4c
Sainandha <nandha8987@gmail.com>
2026-03-04 12:08:11 +0530

    Added more products and UI improvements

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+
 app.use(cors());
 
 const products = [
-    { id: 1, name: "Echo Dot (5th Gen)", price: 49.99, image: "https://m.media-amazon.com/images/I/71YID7jSkwL.jpg" },
-    { id: 2, name: "Kindle Paperwhite", price: 139.99, image: "https://m.media-amazon.com/images/I/71X86VfS6OL.jpg" },
-    { id: 3, name: "Fire TV Stick 4K", price: 49.99, image: "https://m.media-amazon.com/images/I/51v8uxZ92vL.jpg" }
+    { id: 1, name: 'Apple iPhone 15 Pro', price: 999.99, image: 'https://m.media-amazon.com/images/I/81Sig6biNGL._AC_SL1500_.jpg' },
+    { id: 2, name: 'Sony WH-1000XM5 Headphones', price: 348.00, image: 'https://m.media-amazon.com/images/I/61+9m9RySML._AC_SL1500_.jpg' },
+    { id: 3, name: 'Samsung Galaxy Watch 6', price: 299.00, image: 'https://m.media-amazon.com/images/I/61fSR6UfD1L._AC_SL1500_.jpg' },
+    { id: 4, name: 'Apple MacBook Air M2', price: 1099.00, image: 'https://m.media-amazon.com/images/I/71f5Eu5lJSL._AC_SL1500_.jpg' },
+    { id: 5, name: 'PlayStation 5 Console', price: 499.99, image: 'https://m.media-amazon.com/images/I/51051HiS9OL._AC_SL1500_.jpg' },
+    { id: 6, name: 'Nintendo Switch OLED', price: 349.00, image: 'https://m.media-amazon.com/images/I/61Xp6P-SclL._AC_SL1500_.jpg' }
 ];
 
-app.get('/api/products', (req, res) => res.json(products));
+app.get('/api/products', (req, res) => {
+    res.json(products);
+});
 
-app.listen(3000, () => console.log('Amazon Backend running on port 3000'));
+app.listen(3000, () => console.log('Backend running on port 3000'));
